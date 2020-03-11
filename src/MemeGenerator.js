23a45ae535c34635243cdef76102c60803b25a5b
Andreea Nastase <andreanastase@gmail.com>
2020-03-11 17:41:28 +0100

    Add handlers for input and submit

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -9,6 +9,8 @@ class MemeGenerator extends Component {
       randomImage: 'http://i.imgflip.com/1bij.jpg',
       allMemeImgs: [],
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -20,13 +22,45 @@ class MemeGenerator extends Component {
       });
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const randomNumber = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const randMemeImg = this.state.allMemeImgs[randomNumber].url;
+    this.setState({ randomImage: randMemeImg });
+  }
+
   render() {
     return (
       <div>
-        <form className="meme-form">
-          {}
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+
           <button>Generate</button>
         </form>
+        <div className="meme">
+          <img src={this.state.randomImage} alt=" " />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     );
   }
